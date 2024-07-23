b7ea1b9df5c5fb2ec6d63bde05c82fa35c4ab906
Vraj Thakor <65491856+vraj30@users.noreply.github.com>
2024-07-23 15:58:19 +0530

    Update Login.jsx

--- Fronted/src/components/Login.jsx
+++ Fronted/src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
       toast.success("Login successful!");
       navigate('/home');
     } catch (error) {
-      setError('Invalid credentials. Please try again.');
+      
       toast.error("Invalid credentials. Please try again.");
     }
   };
