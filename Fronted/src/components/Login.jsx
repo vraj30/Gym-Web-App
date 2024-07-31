e223e93104a7b1fe26f61311c5f58c068f54f2e8
Vraj Thakor <65491856+vraj30@users.noreply.github.com>
2024-07-31 14:28:09 +0530

    Update Login.jsx

--- Fronted/src/components/Login.jsx
+++ Fronted/src/components/Login.jsx
@@ -5,26 +5,19 @@ import { toast } from 'react-toastify';
 import { auth, provider, signInWithPopup } from '../firebase.js';
 import './Login.css';
 
-
-
-
 const Login = () => {
-
-  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
   const navigate = useNavigate();
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://gym-app-backend-sm1f.onrender.com/login', { email, password });
+      const response = await axios.post('http://localhost:8080/login', { email, password });
       localStorage.setItem('token', response.data.token);
       toast.success("Login successful!");
       navigate('/home');
     } catch (error) {
-      
       toast.error("Invalid credentials. Please try again.");
     }
   };
@@ -32,17 +25,19 @@ const Login = () => {
   const handleGoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
-      const token = result.user.accessToken; // Get the access token
-      
+      const token = await result.user.getIdToken(); // Get the ID token
+      //console.log("Client-side Token:", token); // Log token for debugging
+
       // Send this token to your backend to register the user
-      const res = await axios.post('https://gym-app-backend-sm1f.onrender.com/auth/google', {
+      const res = await axios.post('http://localhost:8080/auth/google', {
         token,
       });
+      //console.log("Response from server:", res.data); // Log server response for debugging
       localStorage.setItem('token', res.data.token);
       toast.success("Google Sign-In successful!");
       navigate('/home');
     } catch (error) {
-      console.error('Google Sign-In Error:', error);
+      //console.error('Google Sign-In Error:', error);
       toast.error("Google Sign-In failed. Please try again.");
     }
   };
@@ -51,42 +46,40 @@ const Login = () => {
     <div className="login-page">
       <div className="login-form">
         <h2>Login</h2>
-        {error && <p className="error-message">{error}</p>}
         <form onSubmit={handleSubmit}>
-        <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input 
-                type="email" 
-                id="email" 
-                name="email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-                required 
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password:</label>
-              <input 
-                type="password" 
-                id="password" 
-                name="password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
-                required 
-              />
-            </div>
-            <button type="submit" className="login-button">Login</button>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </div>
+          <button type="submit" className="login-button">Login</button>
         </form>
         <div className="register-link">
           Don't have an account? <a href="/register">Register</a>
         </div>
         <div className="google-login">
-  <button onClick={handleGoogleLogin} className="google-login-button">
-    <img src="/googlelogo.png" alt="Google Logo" style={{ width: '20px', marginRight: '8px' }} />
-    Login with Google
-  </button>
-</div>
-
+          <button onClick= {handleGoogleLogin} className="google-login-button">
+            <img src="/googlelogo.png" alt="Google Logo" style={{ width: '20px', marginRight: '8px' }} />
+            Login with Google
+          </button>
+        </div>
       </div>
     </div>
   );
